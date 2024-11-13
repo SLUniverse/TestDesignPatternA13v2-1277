a56c68bc56205247d1ea717de8219e9a349a7eb8
SLUniverse <dkicidy@gmail.com>
2024-11-12 21:33:45 -0500

    ajout de java doc

---
+++ src/main/java/edu/coursA13/org/example/Main.java
@@ -0,0 +1,39 @@
+package edu.coursA13.org.example;
+
+/**
+ * Description de la class
+ *
+ * @version 1.0
+ * @author S Lee
+ *
+ * */
+
+public class Main {
+
+    public static String information; /** Chaine de caracteres contenant de l'information */
+    public static String information2; /** Chaine de caracteres contenant de l'info */
+
+    /**
+     *
+     * @param args est un tableau de parametre dans le cas l'execution en ligne
+     */
+
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+    }
+
+    /**
+     *
+     * @param a int a additionner
+     * @param b int a additionner
+     * @return le resultat de l'addition
+     */
+
+
+    public int add(int a, int b){
+        return a+b;
+    }
+}
+
